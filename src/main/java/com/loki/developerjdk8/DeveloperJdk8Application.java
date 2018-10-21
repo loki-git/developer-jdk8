5640c5fc5c307660203616f3ba2eb9d5bb57f9fa
loki-git <xujiansen.happy@163.com>
2018-10-21 12:23:45 +0800

    项目初始化

---
+++ src/main/java/com/loki/developerjdk8/DeveloperJdk8Application.java
@@ -0,0 +1,12 @@
+package com.loki.developerjdk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperJdk8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperJdk8Application.class, args);
+    }
+}
